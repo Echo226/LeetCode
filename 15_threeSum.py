7bbdaf4ef29c177c92438345ce2088ab68a58b77
Xinting Yao <44983835+Echo226@users.noreply.github.com>
2019-11-04 00:14:52 -0500

    Create 15_threeSum.py

---
+++ 15_threeSum.py
@@ -0,0 +1,39 @@
+# Time Complexity: O(n^2)
+# Space Complexity: O(n)
+''' 怎么证明保证了去重？ '''
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+        print(nums)
+        for cur in range(len(nums)):
+            
+            if nums[cur] > 0: break
+            if cur>0 and nums[cur]==nums[cur-1]: continue   # 重点理解
+
+            left = cur + 1
+            right = len(nums) - 1
+            
+            while left < right:
+                if nums[cur] + nums[left] + nums[right] > 0:
+                    # while left < right and nums[right-1] == nums[right]:
+                    #     right -= 1
+                    right -= 1
+                    
+                elif nums[cur] + nums[left] + nums[right] < 0:
+                    # while left < right and nums[left+1] == nums[left]:
+                    #     left += 1
+                    left += 1
+                
+                else:
+                    res.append([nums[cur], nums[left], nums[right]])
+                    print([nums[cur], nums[left], nums[right]])
+                    while left < right and nums[left+1] == nums[left]:
+                        left += 1
+                    while left < right and nums[right-1] == nums[right]:
+                        right -= 1
+                    left += 1
+                    right -= 1
+        return res
+                
+            
